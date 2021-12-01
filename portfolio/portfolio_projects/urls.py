703ff8c1fd2687784d9d74ed75bb1e33d29b71b9
Georgi Dafov <gs.dafov@gmail.com>
2021-12-02 01:47:58 +0200

    Projects UUID fixed

--- portfolio/portfolio_projects/urls.py
+++ portfolio/portfolio_projects/urls.py
@@ -5,10 +5,10 @@ from portfolio.portfolio_projects import views
 urlpatterns = [
     path('', views.ListProjectsView.as_view(), name='list projects'),
     path('create/', views.CreateProjectView.as_view(), name='create project'),
-    path('delete/<int:pk>/', views.DeleteProjectView.as_view(), name='delete project'),
-    path('edit/<int:pk>/', views.EditProjectView.as_view(), name='edit project'),
-    path('details/<int:pk>', views.ProjectDetailsView.as_view(), name='details project'),
+    path('delete/<slug:pk>/', views.DeleteProjectView.as_view(), name='delete project'),
+    path('edit/<slug:pk>/', views.EditProjectView.as_view(), name='edit project'),
+    path('details/<slug:pk>', views.ProjectDetailsView.as_view(), name='details project'),
 
-    path('comment/<int:pk>', views.CommentProjectView.as_view(), name='comment project'),
-    path('comment/delete/<int:pk>', views.DeleteCommentView.as_view(), name='delete comment'),
+    path('comment/<slug:pk>', views.CommentProjectView.as_view(), name='comment project'),
+    path('comment/delete/<slug:pk>', views.DeleteCommentView.as_view(), name='delete comment'),
 ]
